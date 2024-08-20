2bcff8850b44ad6cc30a6ed82d2adae140d01b19
Raf <rafay.b075@gmail.com>
2024-08-19 20:42:31 -0500

    forgot the header

--- Week 1-4/linear_interpolation.py
+++ Week 1-4/linear_interpolation.py
@@ -1,3 +1,15 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do"
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Names: Rafay Bhatti
+# Allen Parrin Grabowski
+# Aidan Iao
+# Omar Hinojosa
+# Section 505
+# Assignment: 1
+# Date: 08/19/2024
+
 x1 = 10 # t = 10 min
 y1 = 2028 # at t = 10 min, distance is 2,028 km
 
