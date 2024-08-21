e688fd105199214776c35f7407ca00dd4a57abb5
Raf <118494048+raf-aye@users.noreply.github.com>
2024-08-21 23:40:48 +0000

    finished part 2 of linear interpolation

--- Week 1-4/linear_interpolation.py
+++ Week 1-4/linear_interpolation.py
@@ -10,16 +10,31 @@
 # Assignment: 1
 # Date: 08/19/2024
 
-x1 = 10 # t = 10 min
-y1 = 2028 # at t = 10 min, distance is 2,028 km
+import math
+# ============================ PART 1 ============================
 
-x2 = 45 # t = 45 min
-y2 = 23028 # at t = 45 min, distance is 23,028 km
+x1 = 10  # t = 10 min
+y1 = 2028  # at t = 10 min, distance is 2,028 km
+
+x2 = 55  # t = 55 min
+y2 = 23028  # at t = 55 min, distance is 23,028 km
 
 chosen_x = 25
 
 interp = ((y2 - y1) / (x2 - x1)) * (chosen_x - x1) + y1
 
-#hi
+print("Part 1:")
+print("For t = 25 minutes, the position p = " + str(interp) + " kilometers")
 
-print(interp)
+# ============================ PART 2 ============================
+
+radius = 6745
+
+chosen_x = 300  # Changes from 25 mins to 5 hours (300 min)
+circumference = 2 * math.pi * radius
+
+interp = ((y2 - y1) / (x2 - x1)) * (chosen_x - x1) + y1
+
+interp = interp % circumference
+print("Part 2:")
+print("For t = 300 minutes, the position p = " + str(interp) + " kilometers")
